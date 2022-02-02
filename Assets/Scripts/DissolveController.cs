4f62aaab20baf03f1bb02bb0b8b2601f60491fbb
BotsOP <prutmaild@gmail.com>
2022-02-02 14:30:04 +0100

    First commit

---
+++ Assets/Scripts/DissolveController.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class DissolveController : MonoBehaviour
+{
+    public Animator animator;
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    public VisualEffect VFXGraph;
+    public float dissolveRate = 0.02f;
+    public float refreshRate = 0.05f;
+    public float dieDelay = 0.2f;
+
+    private Material[] dissolveMaterials;
+    void Start()
+    {
+        if (VFXGraph != null)
+        {
+            VFXGraph.Stop();
+            VFXGraph.gameObject.SetActive(false);
+        }
+
+        if (skinnedMeshRenderer != null)
+        {
+            dissolveMaterials = skinnedMeshRenderer.materials;
+        }
+        
+        StartCoroutine(Dissolve());
+    }
+
+    void Update()
+    {
+        Debug.Log("hoi");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("hoi2");
+            StartCoroutine(Dissolve());
+        }
+    }
+
+    IEnumerator Dissolve()
+    {
+        yield return new WaitForSeconds(3f);
+        if (animator != null)
+        {
+            animator.SetTrigger("Die");
+        }
+
+        yield return new WaitForSeconds(dieDelay);
+
+        if (VFXGraph != null)
+        {
+            VFXGraph.gameObject.SetActive(true);
+            VFXGraph.Play();
+        }
+        
+        float counter = 0;
+
+        if (dissolveMaterials.Length > 0)
+        {
+            while (dissolveMaterials[0].GetFloat("DissolveAmount_") < 1)
+            {
+                counter += dissolveRate;
+                for (int i = 0; i < dissolveMaterials.Length; i++)
+                {
+                    dissolveMaterials[i].SetFloat("DissolveAmount_", counter);
+                }
+
+                yield return new WaitForSeconds(refreshRate);
+            }
+        }
+        
+        Destroy(gameObject, 1);
+    }
+}
