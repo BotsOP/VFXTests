9a3ca19bdccdab618991e6349544bd326f64410d
LAPTOP-IQ9F4R2Q\prutm <prutmaild@gmail.com>
2022-09-22 16:41:12 +0200

    Finished post processing shockwave effect

--- Assets/WorldPosFragHelper.cs
+++ Assets/WorldPosFragHelper.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 
 public class WorldPosFragHelper : MonoBehaviour
 {
-    [SerializeField] private Material shader;
+    [SerializeField] private Material postPrcossMat;
+    [SerializeField] private Transform source;
+    [SerializeField] private float shockwaveSpeed;
+    [SerializeField] private float shockwaveDist;
+    [SerializeField] private float shockwavePow;
+    private static readonly int PosOrigin = Shader.PropertyToID("_PosOrigin");
+    private static readonly int Distance = Shader.PropertyToID("_Distance");
 
     private void Update()
     {
-        Shader.SetGlobalMatrix("_InverseView", Camera.main.cameraToWorldMatrix);
+        postPrcossMat.SetVector(PosOrigin, source.position);
+        
+        float shockwaveDistance = (float)Math.Pow(Time.time % 1 + 1, shockwavePow) - 1;
+        shockwaveDistance *= shockwaveSpeed;
+        shockwaveDistance %= shockwaveDist;
+        postPrcossMat.SetFloat(Distance, shockwaveDistance);
     }
 
     private void OnDrawGizmos()
     {
-        Shader.SetGlobalMatrix("_InverseView", Camera.main.cameraToWorldMatrix);
+        postPrcossMat.SetVector(PosOrigin, source.position);
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, postPrcossMat);
     }
 }
