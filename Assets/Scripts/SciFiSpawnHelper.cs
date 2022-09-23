c8f21aab064a9af0792f9d77a4f5291e8a5f207e
Denis Dvorkovyy <prutmaild@gmail.com>
2022-09-23 23:23:49 +0200

    Finished scifi object spawner

--- Assets/Scripts/SciFiSpawnHelper.cs
+++ Assets/Scripts/SciFiSpawnHelper.cs
@@ -7,15 +7,21 @@ public class SciFiSpawnHelper : MonoBehaviour
 {
     [SerializeField] private Material shader;
     [SerializeField] private Transform spawnPoint;
-    private static readonly int SpawnPoint = Shader.PropertyToID("_SpawnPoint");
+    [SerializeField] private AnimationCurve curve;
+    [SerializeField][Range(0, 1)] private float lerpValue;
+    [SerializeField] private float lerpSpeed = 1;
+    private static readonly int SpawnPoint = Shader.PropertyToID("_TargetPos");
     
     void Update()
     {
         shader.SetVector(SpawnPoint, spawnPoint.position);
+        float t = ((float)Math.Sin(Time.time * lerpSpeed) + 1) / 2;
+        shader.SetFloat("_Lerp", curve.Evaluate(t));
     }
 
     private void OnDrawGizmos()
     {
         shader.SetVector(SpawnPoint, spawnPoint.position);
+        shader.SetFloat("_Lerp", curve.Evaluate(lerpValue));
     }
 }
