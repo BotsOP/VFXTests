d48cc940ffbcfc55f41cdb46eeac9da02994a4d7
LAPTOP-IQ9F4R2Q\prutm <prutmaild@gmail.com>
2022-10-16 17:27:17 +0200

    Got urp shader normal map and multiple lights support working

--- Assets/Shaders/Environment/Water/StylizedWater.shader
+++ Assets/Shaders/Environment/Water/StylizedWater.shader
@@ -1,54 +1,50 @@
 Shader "NedMakesGames/MyLit" {
     Properties{
-        [Header(Surface options)]
-        [MainTexture] _ColorMap("Color", 2D) = "white" {}
-        [MainColor] _ColorTint("Tint", Color) = (1, 1, 1, 1)
+        [Header(Surface options)] // Creates a text header
+        [MainTexture] _BaseMap("Color", 2D) = "white" {}
+        [MainColor] _BaseColor("Tint", Color) = (1, 1, 1, 1)
+        _BumpScale("Normal scale", Float) = 0
+        _BumpMap("Normal map", 2D) = "bump" {}
+
         _Smoothness("Smoothness", Float) = 0
     }
-
     SubShader {
-        Tags {"RenderType" = "Transparent" "RenderPipeline" = "UniversalPipeline" "Queue" = "Transparent"}
+        Tags {"RenderPipeline" = "UniversalPipeline"}
 
         Pass {
-            Name "ForwardLit"
-            Tags{"LightMode" = "UniversalForward" "RenderType" = "Transparent" "Queue" = "Transparent"}
+            Name "ForwardLit" // For debugging
+            Tags{"LightMode" = "UniversalForward"} // Pass specific tags. 
 
-            Blend SrcAlpha OneMinusSrcAlpha
-            ZWrite off
+            HLSLPROGRAM // Begin HLSL code
 
-            HLSLPROGRAM
-
+            #pragma shader_feature_local _NORMALMAP
             #define _SPECULAR_COLOR
-            #pragma shader_feature_local _ALPHA_CUTOUT
-            #pragma shader_feature_local _DOUBLE_SIDED_NORMALS
-            
+
 #if UNITY_VERSION >= 202120
-            #pragma multi_compile _ _MAIN_LIGHT_SHADOWS _MAIN_LIGHT_SHADOWS_CASCADE
+            #pragma multi_compile _ _MAIN_LIGHT_SHADOWS _MAIN_LIGHT_SHADOWS_CASCADE _MAIN_LIGHT_SHADOWS_SCREEN
+            #pragma multi_compile _ _ADDITIONAL_LIGHTS_VERTEX _ADDITIONAL_LIGHTS
+            #pragma multi_compile_fragment _ _ADDITIONAL_LIGHT_SHADOWS
 #else
             #pragma multi_compile _ _MAIN_LIGHT_SHADOWS
             #pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
 #endif
             #pragma multi_compile_fragment _ _SHADOWS_SOFT
-            
+
             #pragma vertex Vertex
             #pragma fragment Fragment
-            
+
             #include "StylizedWaterLitPass.hlsl"
             ENDHLSL
         }
 
 //        Pass {
+//            // The shadow caster pass, which draws to shadow maps
 //            Name "ShadowCaster"
 //            Tags{"LightMode" = "ShadowCaster"}
 //
-//            ColorMask 0
-//            Cull[_Cull]
+//            ColorMask 0 // No color output, only depth
 //
 //            HLSLPROGRAM
-//
-//            #pragma shader_feature_local _ALPHA_CUTOUT
-//            #pragma shader_feature_local _DOUBLE_SIDED_NORMALS
-//
 //            #pragma vertex Vertex
 //            #pragma fragment Fragment
 //
@@ -57,3 +53,18 @@ Shader "NedMakesGames/MyLit" {
 //        }
     }
 }
+
+//float4 LightingWater(SurfaceOutputWater s, float3 lightDir, half3 viewDir, half atten)
+//{
+//	float4 col = float4(s.albedo, s.alpha);
+//	float ndotl = saturate(dot(s.normal, normalize(lightDir)));
+//	float shadow = ndotl * atten;
+//	col.rgb *= shadow;
+//
+//	half3 h = normalize(lightDir + viewDir);
+//	float nh = max(0, dot(s.normal, h));
+//	float spec = pow(nh, 48);
+//	col.rgb = spec * shadow;
+//
+//	return col;
+//}
--- Assets/Shaders/Environment/Water/StylizedWaterLitPass.hlsl
+++ Assets/Shaders/Environment/Water/StylizedWaterLitPass.hlsl
@@ -1,18 +1,31 @@
-#ifndef MY_LIT_FORWARD_LIT_PASS_INCLUDED
-#define MY_LIT_FORWARD_LIT_PASS_INCLUDED
-
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
-TEXTURE2D(_ColorMap); SAMPLER(sampler_ColorMap);
+TEXTURE2D(_BaseMap); SAMPLER(sampler_BaseMap); 
+TEXTURE2D(_BumpMap); SAMPLER(sampler_BumpMap); 
 
-float4 _ColorMap_ST;
-float4 _ColorTint;
-float _Cutoff;
-float _Smoothness;
+CBUFFER_START(UnityPerMaterial)
+float4 _BaseMap_ST;
+float4 _DetailAlbedoMap_ST;
+half4 _BaseColor;
+half4 _SpecColor;
+half4 _EmissionColor;
+half _Cutoff;
+half _Smoothness;
+half _Metallic;
+half _BumpScale;
+half _Parallax;
+half _OcclusionStrength;
+half _ClearCoatMask;
+half _ClearCoatSmoothness;
+half _DetailAlbedoMapScale;
+half _DetailNormalMapScale;
+half _Surface;
+CBUFFER_END
 
 struct Attributes {
 	float3 positionOS : POSITION;
-	float3 normalOS : NORMAL;
+	float4 tangentOS    : TANGENT;
+	float3 normalOS : NORMAL; 
 	float2 uv : TEXCOORD0;
 };
 
@@ -21,40 +34,115 @@ struct Interpolators {
 	float2 uv : TEXCOORD0;
 	float3 positionWS : TEXCOORD1;
 	float3 normalWS : TEXCOORD2;
+	half4 tangentWS : TEXCOORD3;
+	half4 fogFactorAndVertexLight : TEXCOORD4;
+
+#if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+	float4 shadowCoord              : TEXCOORD5;
+#endif
 };
+
+half3 SampleNormal(float2 uv, TEXTURE2D_PARAM(bumpMap, sampler_bumpMap), half scale = half(1.0))
+{
+#ifdef _NORMALMAP
+	half4 n = SAMPLE_TEXTURE2D(bumpMap, sampler_bumpMap, uv);
+#if BUMP_SCALE_NOT_SUPPORTED
+	return UnpackNormal(n);
+#else
+	return UnpackNormalScale(n, scale);
+#endif
+#else
+	return half3(0.0h, 0.0h, 1.0h);
+#endif
+}
+
+void InitializeInputData(Interpolators input, half3 normalTS, out InputData inputData)
+{
+    inputData = (InputData)0;
+
+#if defined(REQUIRES_WORLD_SPACE_POS_INTERPOLATOR)
+    inputData.positionWS = input.positionWS;
+#endif
+
+    half3 viewDirWS = GetWorldSpaceNormalizeViewDir(input.positionWS);
+#if defined(_NORMALMAP) || defined(_DETAIL)
+    float sgn = input.tangentWS.w;      // should be either +1 or -1
+    float3 bitangent = sgn * cross(input.normalWS.xyz, input.tangentWS.xyz);
+    half3x3 tangentToWorld = half3x3(input.tangentWS.xyz, bitangent.xyz, input.normalWS.xyz);
+#if defined(_NORMALMAP)
+    inputData.tangentToWorld = tangentToWorld;
+#endif
+    inputData.normalWS = TransformTangentToWorld(normalTS, tangentToWorld);
+#else
+    inputData.normalWS = input.normalWS;
+#endif
+
+    inputData.normalWS = NormalizeNormalPerPixel(inputData.normalWS);
+    inputData.viewDirectionWS = viewDirWS;
+
+#if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+    inputData.shadowCoord = input.shadowCoord;
+#elif defined(MAIN_LIGHT_CALCULATE_SHADOWS)
+    inputData.shadowCoord = TransformWorldToShadowCoord(inputData.positionWS);
+#else
+    inputData.shadowCoord = float4(0, 0, 0, 0);
+#endif
+	
+#ifdef _ADDITIONAL_LIGHTS_VERTEX
+    inputData.fogCoord = InitializeInputDataFog(float4(input.positionWS, 1.0), input.fogFactorAndVertexLight.x);
+    inputData.vertexLighting = input.fogFactorAndVertexLight.yzw;
+#endif
+
+    inputData.normalizedScreenSpaceUV = GetNormalizedScreenSpaceUV(input.positionCS);
+    inputData.shadowMask = SAMPLE_SHADOWMASK(input.staticLightmapUV);
+
+#if defined(DEBUG_DISPLAY)
+    inputData.vertexSH = input.vertexSH;
+#endif
+}
 
 Interpolators Vertex(Attributes input) {
 	Interpolators output;
 
-	// Found in URP/ShaderLib/ShaderVariablesFunctions.hlsl
 	VertexPositionInputs posnInputs = GetVertexPositionInputs(input.positionOS);
 	VertexNormalInputs normInputs = GetVertexNormalInputs(input.normalOS);
-	
+
+	half3 vertexLight = VertexLighting(posnInputs.positionWS, normInputs.normalWS);
+	half fogFactor = ComputeFogFactor(posnInputs.positionCS.z);
+
+	real sign = input.tangentOS.w * GetOddNegativeScale();
+	half4 tangentWS = half4(normInputs.tangentWS.xyz, sign);
+	output.tangentWS = tangentWS;
+
 	output.positionCS = posnInputs.positionCS;
-	output.uv = TRANSFORM_TEX(input.uv, _ColorMap);
+	output.uv = TRANSFORM_TEX(input.uv, _BaseMap);
 	output.normalWS = normInputs.normalWS;
 	output.positionWS = posnInputs.positionWS;
+	output.fogFactorAndVertexLight = half4(fogFactor, vertexLight);
 
 	return output;
 }
 
-float4 Fragment(Interpolators input) : SV_TARGET {
+float4 Fragment(Interpolators input) : SV_TARGET{
 	float2 uv = input.uv;
-	float4 colorSample = SAMPLE_TEXTURE2D(_ColorMap, sampler_ColorMap, uv);
-
-	float3 normalWS = normalize(input.normalWS);
-	
-	InputData lightingInput = (InputData)0; // Found in URP/ShaderLib/Input.hlsl
-	lightingInput.positionWS = input.positionWS;
-	lightingInput.normalWS = normalWS;
-	lightingInput.viewDirectionWS = GetWorldSpaceNormalizeViewDir(input.positionWS); // In ShaderVariablesFunctions.hlsl
-	lightingInput.shadowCoord = TransformWorldToShadowCoord(input.positionWS); // In Shadows.hlsl
+	// Sample the color map
+	float4 colorSample = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, uv);
 	
 	SurfaceData surfaceInput = (SurfaceData)0;
-	surfaceInput.albedo = colorSample.rgb * _ColorTint.rgb;
-	surfaceInput.alpha = colorSample.a * _ColorTint.a;
+	surfaceInput.albedo = colorSample.rgb * _BaseColor.rgb;
+	surfaceInput.alpha = colorSample.a * _BaseColor.a;
 	surfaceInput.specular = 1;
 	surfaceInput.smoothness = _Smoothness;
+	surfaceInput.normalTS = SampleNormal(uv, TEXTURE2D_ARGS(_BumpMap, sampler_BumpMap), _BumpScale);
+
+	InputData lightingInput = (InputData)0; // Found in URP/ShaderLib/Input.hlsl
+	InitializeInputData(input, surfaceInput.normalTS, lightingInput);
+	// lightingInput.positionWS = input.positionWS;
+	// lightingInput.normalWS = normalize(input.normalWS);
+	// lightingInput.viewDirectionWS = GetWorldSpaceNormalizeViewDir(input.positionWS); // In ShaderVariablesFunctions.hlsl
+	// lightingInput.shadowCoord = TransformWorldToShadowCoord(input.positionWS); // In Shadows.hlsl
+	
+	return UniversalFragmentPBR(lightingInput, surfaceInput);
 
 #if UNITY_VERSION >= 202120
 	return UniversalFragmentBlinnPhong(lightingInput, surfaceInput);
@@ -62,5 +150,3 @@ float4 Fragment(Interpolators input) : SV_TARGET {
 	return UniversalFragmentBlinnPhong(lightingInput, surfaceInput.albedo, float4(surfaceInput.specular, 1), surfaceInput.smoothness, surfaceInput.emission, surfaceInput.alpha);
 #endif
 }
-
-#endif
