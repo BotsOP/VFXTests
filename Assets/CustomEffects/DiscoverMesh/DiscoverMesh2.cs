fbe806beb3df0de52e46311acacbc17a9014ceba
Denis Dvorkovyy <prutmaild@gmail.com>
2022-11-21 18:23:00 +0100

    Got the new discover mesh algorithm working

--- Assets/CustomEffects/DiscoverMesh/DiscoverMesh2.cs
+++ Assets/CustomEffects/DiscoverMesh/DiscoverMesh2.cs
@@ -6,13 +6,15 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class DiscoverMesh2 : MonoBehaviour
+public class DiscoverMesh2 : MonoBehaviour, IHittable
 {
     [SerializeField] private ComputeShader discoverMeshShader;
     [SerializeField] private MeshFilter meshFilter;
     [SerializeField] private int speed;
     [SerializeField] private bool start;
     [SerializeField] private int[] whichTriangleToColor;
+    [SerializeField] private float distThreshold;
+    [SerializeField] private Transform ball;
     
     private GraphicsBuffer gpuVertices;
     private GraphicsBuffer gpuIndices;
@@ -20,6 +22,10 @@ public class DiscoverMesh2 : MonoBehaviour
     private ComputeBuffer gpuTrianglesShouldCheck;
     private ComputeBuffer gpuTrianglesShouldCheckAppend;
     private ComputeBuffer gpuAmountTrianglesToCheck;
+    
+    private ComputeBuffer gpuAmountTrianglesInProximity;
+    private ComputeBuffer gpuTrianglesInProximity;
+    
     private ComputeBuffer debug;
 
     private Mesh targetMesh;
@@ -28,6 +34,7 @@ public class DiscoverMesh2 : MonoBehaviour
     private int threadGroupSize;
     private int checkedIndicesCache;
     private bool doneDiscover;
+    private int[] whichTrianglesToCheck;
     private int amountTriangles => targetMesh.triangles.Length / 3;
     void OnDisable()
     {
@@ -37,6 +44,16 @@ public class DiscoverMesh2 : MonoBehaviour
         gpuIndices = null;
         gpuAdjacentTriangle?.Dispose();
         gpuAdjacentTriangle = null;
+        gpuTrianglesShouldCheck?.Dispose();
+        gpuTrianglesShouldCheck = null;
+        gpuTrianglesShouldCheckAppend?.Dispose();
+        gpuTrianglesShouldCheckAppend = null;
+        gpuAmountTrianglesToCheck?.Dispose();
+        gpuAmountTrianglesToCheck = null;
+        gpuAmountTrianglesInProximity?.Dispose();
+        gpuAmountTrianglesInProximity = null;
+        gpuTrianglesInProximity?.Dispose();
+        gpuTrianglesInProximity = null;
     }
     private void Awake()
     {
@@ -56,6 +73,11 @@ public class DiscoverMesh2 : MonoBehaviour
         gpuAdjacentTriangle = new ComputeBuffer(amountTriangles, sizeof(int) * 4, ComputeBufferType.Structured);
         gpuTrianglesShouldCheckAppend = new ComputeBuffer(amountTriangles * 3, sizeof(int), ComputeBufferType.Append);
         gpuAmountTrianglesToCheck = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Structured);
+        
+        gpuAmountTrianglesInProximity = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Structured);
+        gpuTrianglesInProximity = new ComputeBuffer(amountTriangles, sizeof(int) * 2, ComputeBufferType.Append);
+        TriangleInProximity[] emptyTri = new TriangleInProximity[amountTriangles];
+        gpuTrianglesInProximity.SetData(emptyTri);
 
         debug = new ComputeBuffer(amountTriangles, 4, ComputeBufferType.Structured);
         int[] empty = new int[amountTriangles];
@@ -88,7 +110,8 @@ public class DiscoverMesh2 : MonoBehaviour
         // for (int i = 0; i < amountTriangles; i++)
         // {
         //     if (adjacentTrianglesArray[i].tri1TriangleIndex == -1 ||
-        //         adjacentTrianglesArray[i].tri2TriangleIndex == -1 || adjacentTrianglesArray[i].tri3TriangleIndex == -1)
+        //         adjacentTrianglesArray[i].tri2TriangleIndex == -1 ||
+        //         adjacentTrianglesArray[i].tri3TriangleIndex == -1)
         //     {
         //         Debug.Log($"{i} not 3 connecting triangles");
         //     }
@@ -101,32 +124,25 @@ public class DiscoverMesh2 : MonoBehaviour
         amountTrianglesToCheckArray[0] = 1;
         gpuAmountTrianglesToCheck.SetData(amountTrianglesToCheckArray);
         
-        int[] whichTrianglesToCheck = new int[1];
+        whichTrianglesToCheck = new int[1];
         whichTrianglesToCheck[0] = 0;
         gpuTrianglesShouldCheckAppend.SetData(whichTrianglesToCheck);
     }
 
     public void IncrementTriangles()
     {
-        AdjacentTriangles[] adjacentTrianglesArray = new AdjacentTriangles[amountTriangles];
-        gpuAdjacentTriangle.GetData(adjacentTrianglesArray);
-        
         int[] amountTrianglesToCheckArray = new int[1];
         gpuAmountTrianglesToCheck.GetData(amountTrianglesToCheckArray);
         int amountTrianglesToCheck = amountTrianglesToCheckArray[0];
         if (amountTrianglesToCheck == 0)
         {
+            start = false;
             return;
         }
-        
-        int[] debugInfo = new int[amountTriangles];
-        debug.GetData(debugInfo);
-        int[] debugInfoEmpty = new int[amountTriangles];
-        debug.SetData(debugInfoEmpty);
-        
+
         gpuTrianglesShouldCheck?.Release();
         gpuTrianglesShouldCheck = new ComputeBuffer(amountTrianglesToCheck, sizeof(int), ComputeBufferType.Structured);
-        int[] whichTrianglesToCheck = new int[amountTrianglesToCheck];
+        whichTrianglesToCheck = new int[amountTrianglesToCheck];
         gpuTrianglesShouldCheckAppend.GetData(whichTrianglesToCheck);
         whichTrianglesToCheck = whichTrianglesToCheck.Distinct().ToArray();
         amountTrianglesToCheck = whichTrianglesToCheck.Length;
@@ -142,89 +158,119 @@ public class DiscoverMesh2 : MonoBehaviour
         
         kernelID = discoverMeshShader.FindKernel("DiscoverMesh");
         discoverMeshShader.GetKernelThreadGroupSizes(kernelID, out uint threadGroupSizeX, out _, out _);
-        threadGroupSize = Mathf.CeilToInt(amountTrianglesToCheck / (int)threadGroupSizeX);
-        if (threadGroupSize == 0)
-        {
-            threadGroupSize = 1;
-        }
-        
+        threadGroupSize = Mathf.CeilToInt(amountTrianglesToCheck / (float)threadGroupSizeX);
+
         discoverMeshShader.SetBuffer(kernelID,"gpuVertices", gpuVertices);
         discoverMeshShader.SetBuffer(kernelID,"gpuIndices", gpuIndices);
         discoverMeshShader.SetBuffer(kernelID,"gpuAdjacentTriangle", gpuAdjacentTriangle);
         discoverMeshShader.SetBuffer(kernelID,"gpuTrianglesShouldCheck", gpuTrianglesShouldCheck);
         discoverMeshShader.SetBuffer(kernelID,"gpuTrianglesShouldCheckAppend", gpuTrianglesShouldCheckAppend);
         discoverMeshShader.SetBuffer(kernelID,"gpuAmountTrianglesToCheck", gpuAmountTrianglesToCheck);
-        discoverMeshShader.SetBuffer(kernelID,"debug", debug);
         discoverMeshShader.SetFloat("currentTime", Time.timeSinceLevelLoad);
         discoverMeshShader.SetInt("amountTrianglesToCheck", amountTrianglesToCheck);
         discoverMeshShader.SetInt("amountTriangles", amountTriangles);
         gpuTrianglesShouldCheckAppend.SetCounterValue(0);
         discoverMeshShader.Dispatch(kernelID, threadGroupSize, 1, 1);
+    }
+
+    public void Hit(Vector3 hitPos)
+    {
+        TriangleInProximity[] trisEmpty = new TriangleInProximity[amountTriangles];
+        gpuTrianglesInProximity.SetData(trisEmpty);
+        int[] empty = new int[1];
+        gpuAmountTrianglesInProximity.SetData(empty);
         
+        kernelID = discoverMeshShader.FindKernel("FindClosestTriangle");
+        discoverMeshShader.GetKernelThreadGroupSizes(kernelID, out uint threadGroupSizeX, out _, out _);
+        threadGroupSize = Mathf.CeilToInt(amountTriangles / (float)threadGroupSizeX);
         
+        gpuVertices ??= targetMesh.GetVertexBuffer(0);
+        gpuIndices ??= targetMesh.GetIndexBuffer();
+
+        Vector3 pos = meshFilter.transform.worldToLocalMatrix.MultiplyPoint3x4(hitPos);
+        ball.localPosition = pos;
+        
+        gpuTrianglesInProximity.SetCounterValue(0);
+        discoverMeshShader.SetBuffer(kernelID,"gpuVertices", gpuVertices);
+        discoverMeshShader.SetBuffer(kernelID,"gpuIndices", gpuIndices);
+        discoverMeshShader.SetBuffer(kernelID,"gpuAmountTrianglesInProximity", gpuAmountTrianglesInProximity);
+        discoverMeshShader.SetBuffer(kernelID,"gpuTrianglesInProximity", gpuTrianglesInProximity);
+        discoverMeshShader.SetFloat("distThreshold", distThreshold);
+        discoverMeshShader.SetVector("hitPos", pos);
+        discoverMeshShader.SetInt("amountTriangles", amountTriangles);
+        discoverMeshShader.Dispatch(kernelID, threadGroupSize, 1, 1);
+
+        int[] amountTrianglesInProximityArray = new int[1];
+        gpuAmountTrianglesInProximity.GetData(amountTrianglesInProximityArray);
+        int amountTrianglesInProximity = amountTrianglesInProximityArray[0];
+        
+        TriangleInProximity[] tris = new TriangleInProximity[amountTrianglesInProximity];
+        gpuTrianglesInProximity.GetData(tris);
+        TriangleInProximity lowestDistTri = new TriangleInProximity(0, 99999);
+        foreach (TriangleInProximity tri in tris)
+        {
+            if (tri.dist < lowestDistTri.dist)
+            {
+                lowestDistTri = tri;
+            }
+        }
+        Debug.Log($"id: {lowestDistTri.id}");
     }
 
     public void ColorTriangles()
     {
         targetMesh = meshFilter.sharedMesh;
-        Vector3[] vertices = targetMesh.vertices;
+        int amountTrianglesToColor = whichTriangleToColor.Length;
         int[] indices = targetMesh.triangles;
-        Vector2[] uvs = new Vector2[targetMesh.vertices.Length];
-        for (int i = 0; i < amountTriangles; i++)
+        Vector2[] uvs = new Vector2[targetMesh.vertexCount];
+        Vertex[] vertices = new Vertex[targetMesh.vertexCount];
+        gpuVertices ??= targetMesh.GetVertexBuffer(0);
+        gpuVertices.GetData(vertices);
+        for (int i = 0; i < targetMesh.vertexCount; i++)
         {
-            int index1 = indices[i * 3];
-            int index2 = indices[i * 3 + 1];
-            int index3 = indices[i * 3 + 2];
+            uvs[i] = new Vector2(vertices[i].uv.x, 0);
+        }
         
-            Vector3 vertex1 = vertices[index1];
-            Vector3 vertex2 = vertices[index2];
-            Vector3 vertex3 = vertices[index3];
+        for (int i = 0; i < amountTrianglesToColor; i++)
+        {
+            int index1 = indices[whichTriangleToColor[i] * 3];
+            int index2 = indices[whichTriangleToColor[i] * 3 + 1];
+            int index3 = indices[whichTriangleToColor[i] * 3 + 2];
         
-            Vector3[] allPos = new Vector3[3];
-            allPos[0] = vertices[index1];
-            allPos[0] = vertices[index2];
-            allPos[0] = vertices[index3];
-
-            for (int j = 0; j < whichTriangleToColor.Length; j++)
-            {
-                if (i == whichTriangleToColor[j])
-                {
-                    uvs[index1] = Vector2.up;
-                    uvs[index2] = Vector2.up;
-                    uvs[index3] = Vector2.up;
-                }
-            }
+            uvs[index1].y = 1;
+            uvs[index2].y = 1;
+            uvs[index3].y = 1;
         }
         targetMesh.uv = uvs;
     }
 
     private void Update()
     {
-        if (start)
+        if (start && Time.time > speed)
         {
             if (!firstTimeStart)
             {
                 FirstTriangleToCheck();
                 firstTimeStart = true;
             }
-            IncrementTriangles();
-        }
-    }
-
-    private bool DistanceLessThen(Vector3[] a, Vector3 b, float maxDist)
-    {
-        for (int i = 0; i < a.Length; i++)
-        {
-            if (math.abs(a[i].x - b.x) < maxDist && 
-                math.abs(a[i].y - b.y) < maxDist && 
-                math.abs(a[i].z - b.z) < maxDist)
+            while (start)
             {
-                return true;
+                IncrementTriangles();
             }
         }
-
-        return false;
     }
+}
+
+struct TriangleInProximity
+{
+    public TriangleInProximity(uint _id, float _dist)
+    {
+        id = _id;
+        dist = _dist;
+    }
+    
+    public uint id;
+    public float dist;
 }
 
 struct CheckedTriangle
